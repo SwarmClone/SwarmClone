7045e3f860769d3b6adc794e2bb39f730988d01d
kyv001 <kyvyang@foxmail.com>
2025-01-22 17:06:01 +0800

    防止在不应出现的地方出现分隔符

--- swarmclone/request_parser.py
+++ swarmclone/request_parser.py
@@ -8,6 +8,8 @@ def loads(request_str: str) -> list[dict]:
     request_strings = request_str.split(config.REQUESTS_SEPARATOR)
     requests = []
     for request_string in request_strings:
+        if not request_string:
+            continue
         try:
             requests.append(json.loads(request_string))
         except json.JSONDecodeError:
@@ -15,4 +17,8 @@ def loads(request_str: str) -> list[dict]:
     return requests
 
 def dumps(requests: list[dict]) -> str:
-    return "".join([json.dumps(request) + config.REQUESTS_SEPARATOR for request in requests])
+    return "".join([
+        (json.dumps(request).replace(config.REQUESTS_SEPARATOR, "") + # 防止在不应出现的地方出现分隔符
+        config.REQUESTS_SEPARATOR)
+        for request in requests
+    ])
