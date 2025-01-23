2b72bb61fb3cc2485d42eaee2e47a9b268e0bb52
kyv001 <kyvyang@foxmail.com>
2025-01-23 17:05:53 +0800

    修改逻辑错误*1

--- swarmclone/model_qwen/__main__.py
+++ swarmclone/model_qwen/__main__.py
@@ -73,9 +73,12 @@ if __name__ == '__main__':
     abs_model_path = os.path.expanduser(qwen2_config.MODEL_PATH)
     while not successful:
         try:
+            print(f"正在从{abs_model_path}加载模型……")
             model = AutoModelForCausalLM.from_pretrained(abs_model_path, torch_dtype="auto", device_map="auto")
             tokenizer = AutoTokenizer.from_pretrained(abs_model_path, padding_side="left")
-        except:
+            successful = True
+        except Exception as e:
+            print(e)
             choice = input("加载模型失败，是否下载模型？(Y/n)")
             if choice.lower() != "n":
                 import huggingface_hub # type: ignore
