535e720b5c8597e4a7d18e4b7a76f55aec8f1845
kyv001 <kyvyang@foxmail.com>
2025-01-23 22:57:20 +0800

    正确使用subprocess.run

--- get_cosyvoice_reqs.py
+++ get_cosyvoice_reqs.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import subprocess
 
@@ -8,14 +7,21 @@ install = input(" * 确认? [y/n]")
 if install.strip().lower() == "y":
     print(" * 开始添加 Minimum CosyVoice: ")
     if sys.platform.startswith("linux"):
-        subprocess.run("python -m pip install http://pixelstonelab.com:5005/ttsfrd-0.4.2-cp310-cp310-linux_x86_64.whl http://pixelstonelab.com:5005/ttsfrd_dependency-0.1-py3-none-any.whl")
+        subprocess.run([
+            "python", "-m", "pip", "install",
+            "http://pixelstonelab.com:5005/ttsfrd-0.4.2-cp310-cp310-linux_x86_64.whl",
+            "http://pixelstonelab.com:5005/ttsfrd_dependency-0.1-py3-none-any.whl"
+        ])
     else:
-        subprocess.run("conda install -c conda-forge pynini==2.1.6 -y")
-    subprocess.run("python -m pip install http://pixelstonelab.com:5005/sc_cosyvoice-0.1.0-py3-none-any.whl spacy-pkuseg dragonmapper hanziconv")
+        subprocess.run(["conda", "install", "-c", "conda-forge", "pynini==2.1.6", "-y"])
+    subprocess.run([
+        "python", "-m", "pip", "install",
+        "http://pixelstonelab.com:5005/sc_cosyvoice-0.1.0-py3-none-any.whl",
+        "spacy-pkuseg", "dragonmapper", "hanziconv"])
 
-    os.system("clear" if sys.platform.startswith("linux") else "cls")
+    print("\033[H\033[J")
     print(" * 开始添加 Montreal Forced-Aligner: ")
-    subprocess.run("conda install -c conda-forge montreal-forced-aligner -y")
+    subprocess.run(["conda", "install", "-c", "conda-forge", "montreal-forced-aligner", "-y"])
 
     print(" # 添加完毕！")
 else:
