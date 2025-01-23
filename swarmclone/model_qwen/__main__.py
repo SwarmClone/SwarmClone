3eb022e47184360fbb35c38d52f30b37d2ff2dd7
kyv001 <kyvyang@foxmail.com>
2025-01-23 13:19:11 +0800

    让qwen模型适配新API

--- swarmclone/model_qwen/__main__.py
+++ swarmclone/model_qwen/__main__.py
@@ -3,44 +3,79 @@ import threading
 import json
 import queue
 import os
-from transformers import pipeline, TextIteratorStreamer # type: ignore
+import re
+import uuid
+import time
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer, StoppingCriteria # type: ignore
 from . import config, qwen2_config
+from ..request_parser import *
 
-q: queue.Queue[dict[str, str]] = queue.Queue()
+class CustomStoppingCriteria(StoppingCriteria):
+    def __init__(self, stop_event: threading.Event, eos_token_id: int):
+        self.stop_event = stop_event
+        self.eos_token_id = eos_token_id
 
-def recv_msg(sock: socket.socket, q: queue.Queue[dict[str, str]]):
+    def __call__(self, input_ids, scores) -> bool: # input_ids和scores因为不想为了类型单独导入torch所以没有类型提示
+        if self.stop_event.is_set(): # 在需要时可以直接停止生成
+            return True
+        if input_ids[0][-1] == self.eos_token_id:
+            return True
+        return False
+
+def split_text(text: str, separators: list[str]) -> list[str]:
+    return [part for part in re.split("|".join(separators), text) if part.strip()]
+
+q_recv: queue.Queue[RequestType] = queue.Queue()
+def recv_msg(sock: socket.socket, q: queue.Queue[RequestType], stop_module: threading.Event):
     while True:
-        try:
-            data = json.loads(sock.recv(1024).decode())
-        except json.JSONDecodeError:
-            pass
-        else:
-            if data["from"] == "stop":
-                q.put({"role": "stop", "content": ""})
-                break
-            if data["from"] == "ASR":
-                q.put({"role": "user", "content": data["text"]})
-            else:
-                for message in data["list"]:
-                    q.put({"role": "user", "content": message["text"]})
+        data = sock.recv(1024)
+        if not data:
+            break
+        messages = loads(data.decode())
+        for message in messages:
+            q.put(message)
+
+q_send: queue.Queue[RequestType] = queue.Queue()
+def send_msg(sock: socket.socket, q: queue.Queue[RequestType], stop_module: threading.Event):
+    while True:
+        message = q.get()
+        data = dumps([message]).encode()
+        sock.sendall(data)
+
+def generate(model: AutoModelForCausalLM, text_inputs: list[dict[str, str]], streamer: TextIteratorStreamer):
+    try:
+        text = tokenizer.apply_chat_template(text_inputs, tokenizer=False, add_generation_prompt=True)
+        model_inputs = tokenizer(text, return_tensors="pt").to(model.device)
+        model.generate(
+            model_inputs,
+            max_new_tokens=512,
+            streamer=streamer,
+            stopping_criteria=CustomStoppingCriteria(stop_generation, tokenizer.eos_token_id)
+        )
+    except Exception as e:
+        print(e)
+        stop_generation.set()
+
+# 状态
+STANDBY = 0
+GENERATE = 1
+WAIT_FOR_TTS = 2
+WAIT_FOR_ASR = 3
+
+# 事件
+stop_generation = threading.Event()
+stop_module = threading.Event()
 
 if __name__ == '__main__':
     successful = False
     abs_model_path = os.path.expanduser(qwen2_config.MODEL_PATH)
     while not successful:
         try:
-            pipe = pipeline(
-                'text-generation',
-                model=abs_model_path,
-                torch_dtype="auto",
-                device_map="auto",
-                max_new_tokens=1000,
-            )
-            successful = True
+            model = AutoModelForCausalLM.from_pretrained(abs_model_path, torch_dtype="auto", device_map="auto")
+            tokenizer = AutoTokenizer.from_pretrained(abs_model_path, padding_side="left")
         except:
             choice = input("加载模型失败，是否下载模型？(Y/n)")
             if choice.lower() != "n":
-                import os
                 import huggingface_hub # type: ignore
                 huggingface_hub.snapshot_download(
                     repo_id=qwen2_config.MODEL_ID,
@@ -48,39 +83,100 @@ if __name__ == '__main__':
                     local_dir=abs_model_path,
                     endpoint="https://hf-mirror.com"
                 )
-    streamer = TextIteratorStreamer(pipe.tokenizer, skip_prompt=True, skip_special_tokens=True)
-    with (  socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_input,
-            socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_output):
-        sock_input.connect((config.PANEL_HOST, config.PANEL_TO_LLM))
-        sock_output.connect((config.PANEL_HOST, config.PANEL_FROM_LLM))
-        t_recv = threading.Thread(target=recv_msg, args=(sock_input, q))
+    
+    streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((config.PANEL_HOST, config.LLM_PORT))
+        t_recv = threading.Thread(target=recv_msg, args=(sock, q_recv, stop_module))
         t_recv.start()
-        messages: list[dict[str, str]] = []
-        while True:
-            message = q.get(True)
-            if message["role"] == "stop":
-                sock_output.sendall(json.dumps({"from": "stop"}).encode())
-                sock_output.close()
-                sock_input.close()
-                break
-            messages.append(message)
-            kwargs = {"text_inputs": messages, "streamer": streamer}
-            generation_thread = threading.Thread(target=pipe, kwargs=kwargs)
-            generation_thread.start()
-            generated_text = ""
-            for text in streamer:
-                data = {
-                    "from": "LLM",
-                    "token": text,
-                    "feelings": {}
-                }
-                sock_output.sendall(json.dumps(data).encode())
-                generated_text += text
-            data = {
-                "from": "LLM",
-                "token": "<eos>",
-                "feelings": {}
-            } # 表示生成结束
-            sock_output.sendall(json.dumps(data).encode())
-            messages.append({"role": "assistant", "content": generated_text})
-        
+        t_send = threading.Thread(target=send_msg, args=(sock, q_send, stop_module))
+        t_send.start()
+        generation_thread: threading.Thread | None = None # 在没有生成任务前没有值
+
+        history: list[dict[str, str]] = []
+        state = STANDBY
+        text = ""
+        standby_time = time.time()
+        while True: # 状态机
+            """
+            待机状态：
+             - 若处于待机状态时间大于5s，切换到生成状态
+             - 若收到ASR给出的语音活动信号，切换到等待ASR状态
+            生成状态：
+             - 生成一段回复完毕后切换到等待TTS状态
+             - 若收到ASR给出的语音活动信号，切换到等待ASR状态
+             - 从生成状态切换到其他状态时发出一个<eos>信号
+            等待TTS状态：
+             - 若收到TTS给出的生成完毕信号，切换到待机状态
+            等待ASR状态：
+            - 若收到ASR给出的语音识别信息，切换到生成状态
+            """
+            try:
+                message: RequestType | None = q_recv.get(False)
+            except queue.Empty:
+                message = None
+            match state:
+                case STANDBY:
+                    if time.time() - standby_time > 5:
+                        stop_generation.clear()
+                        history.append({'role': 'user', 'content': '请随便说点什么吧！'})
+                        kwargs = {"model": model, "text_inputs": history, "streamer": streamer}
+                        generation_thread = threading.Thread(target=generate, kwargs=kwargs)
+                        generation_thread.start()
+                        state = GENERATE
+                        text = ""
+                        continue
+                    if message == ASR_ACTIVATE:
+                        state = WAIT_FOR_ASR
+
+                case GENERATE:
+                    try:
+                        text += next(streamer)
+                    except StopIteration:
+                        q_send.put(LLM_EOS)
+                        state = WAIT_FOR_TTS
+                        stop_generation.set()
+                        if generation_thread is not None and generation_thread.is_alive():
+                            generation_thread.join()
+                        continue
+                    if message == ASR_ACTIVATE:
+                        q_send.put(LLM_EOS)
+                        state = WAIT_FOR_ASR
+                        stop_generation.set()
+                        if generation_thread is not None and generation_thread.is_alive():
+                            generation_thread.join()
+                        continue
+                    *sentences, text = split_text(text, ".!?。？！…\n\r") # 将所有完整的句子发送
+                    for i, sentence in enumerate(sentences):
+                        q_send.put({
+                            'from': 'llm',
+                            'type': 'data',
+                            'payload': {
+                                'content': sentence,
+                                'id': str(uuid.uuid4())
+                            }
+                        })
+
+                case WAIT_FOR_ASR:
+                    if message is not None and message['from'] == 'asr' and message['type'] == 'data':
+                        stop_generation.clear()
+                        history.append({'role': 'user', 'content': message['payload']['content']})
+                        kwargs = {"model": model, "text_inputs": history, "streamer": streamer}
+                        generation_thread = threading.Thread(target=generate, kwargs=kwargs)
+                        generation_thread.start()
+                        state = GENERATE
+                        text = ""
+                        continue
+
+                case WAIT_FOR_TTS:
+                    if message == TTS_FINISH:
+                        state = STANDBY
+                        standby_time = time.time()
+                        continue
+            if message is not None and message['type'] == 'signal' and message['payload'] == 'exit':
+                stop_generation.set()
+                stop_module.set()
+        t_recv.join()
+        t_send.join()
+        if generation_thread is not None and generation_thread.is_alive():
+            generation_thread.join()
