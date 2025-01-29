5d1d7ad5b726b287eb8cab3b5fc202e6202176c1
郝某人 <1218271192@qq.com>
2025-01-29 11:56:24 +0800

    创建基础前端界面，重构部分面板后端代码

--- swarmclone/panel_dummy/__main__.py
+++ swarmclone/panel_dummy/__main__.py
@@ -1,11 +1,4 @@
 import socket
-<<<<<<< Updated upstream
-import json
-import subprocess
-import time
-from . import config
-from ..request_parser import *
-=======
 import time
 import webbrowser
 
@@ -13,7 +6,6 @@ from .core.types import ModuleType
 from .core.module_manager import ModuleManager
 from .frontend.service import FrontendService
 from . import config
->>>>>>> Stashed changes
 
 
 def main():
@@ -42,104 +34,6 @@ def main():
     started_event.wait()
     webbrowser.open(f'http://{config.PANEL_HOST}:{config.FRONTEND_PORT}/pages/index.html')
 
-<<<<<<< Updated upstream
-SUBMODULE_NAMES = ["LLM", "ASR", "TTS", "FRONTEND", "CHAT"]
-PORTS = [
-    config.LLM_PORT,
-    config.ASR_PORT,
-    config.TTS_PORT,
-    config.FRONTEND_PORT, 
-    config.CHAT_PORT
-]
-LLM = iota()
-ASR = iota()
-TTS = iota()
-FRONTEND = iota()
-CHAT = iota()
-CONN_TABLE: dict[int, tuple[list[int], list[int]]] = {
-#  发送方       信号接受方               数据接受方
-    LLM:  ([     TTS, FRONTEND], [     TTS, FRONTEND]),
-    ASR:  ([LLM, TTS, FRONTEND], [LLM,      FRONTEND]),
-    TTS:  ([LLM,      FRONTEND], [          FRONTEND]),
-    CHAT: ([                  ], [LLM,      FRONTEND])
-} # 数据包转发表
-CONNECTIONS: list[socket.socket | None] = [None for _ in range(iota.count)]
-
-def handle_submodule(submodule: int, sock: socket.socket) -> None:
-    global CONNECTIONS, running
-    loader = Loader(config)
-    print(f"Waiting for {SUBMODULE_NAMES[submodule]}...")
-    CONNECTIONS[submodule], _ = sock.accept() # 不需要知道连接的地址所以直接丢弃
-    while True: # 等待模块上线
-        data = CONNECTIONS[submodule].recv(1024) # type: ignore
-        if not data:
-            continue
-        loader.update(data.decode())
-        if MODULE_READY in loader.get_requests():
-            break
-        time.sleep(0.1)
-    print(f"{SUBMODULE_NAMES[submodule]} is online.")
-    try:
-        while not running: # 等待启动
-            time.sleep(0.1)
-        CONNECTIONS[submodule].sendall(dumps([PANEL_START]).encode("utf-8")) # type: ignore
-
-        while running:
-            # CONNECTIONS[submodule]必然不会是None
-            data = CONNECTIONS[submodule].recv(1024) # type: ignore
-            if not data:
-                running = False
-                break
-            # 逐个解析请求并将其转发给相应的模块
-            loader.update(data.decode())
-            for request in loader.get_requests():
-                print(f"{SUBMODULE_NAMES[submodule]}: {request}")
-                request_bytes = dumps([request]).encode()
-                for receiver in CONN_TABLE[submodule][request["type"] == "data"]:
-                    if CONNECTIONS[receiver]:
-                        CONNECTIONS[receiver].sendall(request_bytes) # type: ignore
-    except KeyboardInterrupt:
-        running = False
-
-    # 让模块停止并退出
-    if CONNECTIONS[submodule] is not None:
-        CONNECTIONS[submodule].sendall(dumps([PANEL_STOP]).encode("utf-8")) # type: ignore
-        CONNECTIONS[submodule].close() # type: ignore
-        CONNECTIONS[submodule] = None
-
-if __name__ == '__main__':
-    running = False
-
-    # 创建套接字并监听
-    sockets: list[socket.socket] = [
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        for _ in range(iota.count)
-    ]
-    for i, sock in enumerate(sockets):
-        sock.bind((config.PANEL_HOST, PORTS[i]))
-        sock.listen(1)
-
-    # 分别启动处理各个模块的子线程
-    threads: list[threading.Thread] = [
-        threading.Thread(target=handle_submodule, args=t)
-        for t in enumerate(sockets)
-    ]
-    for t in threads:
-        t.start()
-
-    # 只需要LLM、TTS和FRONTEND上线即可开始运行，ASR和CHAT不必需
-    while not all([CONNECTIONS[LLM], CONNECTIONS[TTS], CONNECTIONS[FRONTEND]]):
-        time.sleep(0.1) # 防止把CPU占满
-    
-    print("Start.")
-    running = True
-    for t in threads:
-        t.join()
-
-    # 所有子线程都已退出
-    for s in sockets:
-        s.close()
-=======
     try:
         manager.running = True
         while True:
@@ -152,4 +46,3 @@ if __name__ == '__main__':
 
 if __name__ == "__main__":
     main()
->>>>>>> Stashed changes
