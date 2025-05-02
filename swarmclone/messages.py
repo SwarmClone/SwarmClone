dfbb27628b3eb70af265febcf6bb3f9d0cf5c9f8
kyv001 <kyvyang@foxmail.com>
2025-05-02 14:34:00 +0800

    定义newapi（旧的网络api）中的信号和消息类型

--- swarmclone/messages.py
+++ swarmclone/messages.py
@@ -28,6 +28,9 @@ class Message:
         return self.kwargs
 
 class ASRActivated(Message):
+    """
+    语音活动激活信号，用于打断正在播放的语音和正在生成的回复
+    """
     def __init__(self, source: ModuleBase):
         super().__init__(
             MessageType.SIGNAL,
@@ -36,6 +39,11 @@ class ASRActivated(Message):
         )
 
 class ASRMessage(Message):
+    """
+    语音识别得到的信息
+    .speaker_name: 说话人
+    .message: 语音识别得到的信息
+    """
     def __init__(self, source: ModuleBase, speaker_name: str, message: str):
         super().__init__(
             MessageType.DATA,
@@ -43,4 +51,75 @@ class ASRMessage(Message):
             destinations=[ModuleRoles.LLM, ModuleRoles.FRONTEND],
             speaker_name=speaker_name,
             message=message
+        )
+
+class LLMEOS(Message):
+    """
+    LLM 生成结束信号
+    """
+    def __init__(self, source: ModuleBase):
+        super().__init__(
+            MessageType.SIGNAL,
+            source,
+            destinations=[ModuleRoles.FRONTEND, ModuleRoles.TTS]
+        )
+
+class LLMMessage(Message):
+    """
+    LLM 生成的信息
+    .content：生成的信息
+    .id：消息的 id（uuid）
+    .emotion：情感信息。含有like disgust anger happy sad neutral五个情感的概率
+    """
+    def __init__(self, source: ModuleBase, content: str, id: str, emotion: dict):
+        super().__init__(
+            MessageType.DATA,
+            source,
+            destinations=[ModuleRoles.FRONTEND, ModuleRoles.TTS],
+            content=content,
+            id=id,
+            emotion=emotion
+        )
+
+class TTSFinished(Message):
+    """
+    TTS 结束信号
+    """
+    def __init__(self, source: ModuleBase):
+        super().__init__(
+            MessageType.SIGNAL,
+            source,
+            destinations=[ModuleRoles.FRONTEND, ModuleRoles.LLM]
+        )
+
+class TTSAlignment(Message):
+    """
+    TTS 对齐信息
+    .id：消息的 id（uuid）
+    .token：当前 token
+    .duration：当前 token 的时长
+    """
+    def __init__(self, source: ModuleBase, id: str, token: str, duration: float):
+        super().__init__(
+            MessageType.DATA,
+            source,
+            destinations=[ModuleRoles.FRONTEND],
+            id=id,
+            token=token,
+            duration=duration
+        )
+
+class ChatMessage(Message):
+    """
+    聊天信息
+    .user：用户名
+    .content：消息内容
+    """
+    def __init__(self, source: ModuleBase, user: str, content: str):
+        super().__init__(
+            MessageType.DATA,
+            source,
+            destinations=[ModuleRoles.LLM, ModuleRoles.FRONTEND],
+            user=user,
+            content=content
         )
