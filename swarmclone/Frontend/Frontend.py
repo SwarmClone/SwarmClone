2fe83a135639a8658b3ab5b06b375e050e8ddaca
HH233333 <hh233333334@gmail.com>
2025-05-03 15:56:26 +0800

    实现frontend

---
+++ swarmclone/Frontend/Frontend.py
@@ -0,0 +1,52 @@
+import asyncio
+import json
+import base64
+ 
+from swarmclone.config import config
+from ..modules import ModuleRoles, ModuleBase
+from ..messages import *
+
+class frontend(ModuleBase):
+    def __init__(self):
+        super().__init__(ModuleRoles.FRONTEND, "Frontend")
+        self.clientdict:dict[int,asyncio.StreamWriter] = []
+        self.server = None
+
+    async def run(self):
+        loop = asyncio.get_running_loop()
+        loop.create_task(self.preprocess_tasks())
+        self.server = await asyncio.start_server(self.handle_client(self.clientdict),config.panel.server.host, config.panel.frontend.port)
+        async with self.server:
+            await self.server.serve_forever()
+        
+    
+    async def preprocess_tasks(self):
+        task = await self.task_queue.get()
+        message = self.load(task)
+        for addr, client in self.clientdict:
+            try:
+                client.write(message.encode('utf-8'))
+                await client.drain()
+                print(f"消息已发送给 {addr}")
+            except ConnectionResetError:
+                print(f"客户端 {addr} 已断开连接")
+                client.close()
+                del self.clientdict[addr]
+
+    def handle_client(self, writer:asyncio.StreamWriter) -> None:
+        addr = writer.get_extra_info('peername')
+        print(f"客户端已连接：{addr}")
+        self.clientdict[addr]=writer
+    
+    def load(task:Message) -> str:
+        dict = {
+                "message_type": task.message_type.value,
+                "source": task.source.role.value,
+                **task.kwargs
+            }
+        if(isinstance(task, TTSAudio)):
+            dict["data"] = base64.b64decode(dict["data"]).decode('utf-8')
+        massage = (json.dumps(dict).replace(config.panel.server.requests_separator, "") + # 防止在不应出现的地方出现分隔符
+        config.panel.server.requests_separator)
+        return(massage)
+    
