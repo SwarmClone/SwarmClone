10b4a950c16acb98330d4a0263c0ba4177317a8a
HH233333 <hh233333334@gmail.com>
2025-05-04 11:42:41 +0800

    注册frontend

--- swarmclone/__main__.py
+++ swarmclone/__main__.py
@@ -2,10 +2,11 @@ from .controller import Controller
 from .modules import *
 from .constants import *
 from .tts_cosyvoice import TTSCosyvoice
+from .Frontend import frontend
 
 if __name__ == "__main__":
     controller = Controller()
-    controller.register(FrontendDummy())
+    controller.register(frontend())
     controller.register(TTSCosyvoice())
     controller.register(LLMDummy())
     controller.register(ASRDummy())
