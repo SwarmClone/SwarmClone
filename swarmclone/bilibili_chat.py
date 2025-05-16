47096fe17305273ff1c2e1229d0a593ad1513534
kyv001 <kyvyang@foxmail.com>
2025-05-16 20:38:57 +0800

    优化代码实现

--- swarmclone/bilibili_chat.py
+++ swarmclone/bilibili_chat.py
@@ -9,12 +9,14 @@ from .messages import *
 class BiliBiliChat(ModuleBase):
     def __init__(self):
         super().__init__(ModuleRoles.CHAT, "BiliBiliChat")
-        self.credential = Credential(sessdata =config.chat.bilibili.credential.sessdata if config.chat.bilibili.credential.sessdata else None,
-                                     bili_jct=config.chat.bilibili.credential.bili_jct if config.chat.bilibili.credential.sessdata else None,
-                                     buvid3=config.chat.bilibili.credential.buvid3 if config.chat.bilibili.credential.sessdata else None,
-                                     dedeuserid=config.chat.bilibili.credential.dedeuserid if config.chat.bilibili.credential.sessdata else None,
-                                     ac_time_value=config.chat.bilibili.credential.ac_time_value if config.chat.bilibili.credential.sessdata else None)
-        self.room = live.LiveDanmaku(config.chat.bilibili.live_room_id, credential = self.credential)
+        self.credential = Credential(
+            sessdata=config.chat.bilibili.credential.sessdata or None,
+            bili_jct=config.chat.bilibili.credential.bili_jct or None,
+            buvid3=config.chat.bilibili.credential.buvid3 or None,
+            dedeuserid=config.chat.bilibili.credential.dedeuseride or None,
+            ac_time_value=config.chat.bilibili.credential.ac_time_value or None
+        )
+        self.room = live.LiveDanmaku(config.chat.bilibili.live_room_id, credential=self.credential)
         self.register_chat()
     
     def register_chat(self):
@@ -24,16 +26,12 @@ class BiliBiliChat(ModuleBase):
             print(f"{(user := event['data']['info'][2][1])}: {(msg := event['data']['info'][1])}")
             await self.results_queue.put(
                 ChatMessage(
-                    source = self,
-                    user = event['data']['info'][2][1],
-                    content = event['data']['info'][1],
+                    source=self,
+                    user=user,
+                    content=msg,
                 )
                         
             )
 
     async def run(self):
         await self.room.connect()
-
-    async def process_task(self, task: Message | None) -> Message | None:
-        # 不应被调用
-        raise NotImplementedError
