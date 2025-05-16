0c01373da417305f014401b05951556eb4c5417c
kyv001 <kyvyang@foxmail.com>
2025-05-16 21:25:53 +0800

    默认启动bilibili chat

--- swarmclone/__main__.py
+++ swarmclone/__main__.py
@@ -4,10 +4,12 @@ from .constants import *
 from .tts_cosyvoice import TTSCosyvoice
 from .frontend_socket import FrontendSocket
 from .llm_minilm2 import LLMMiniLM2
+from .bilibili_chat import BiliBiliChat
 
 if __name__ == "__main__":
     controller = Controller()
     controller.register(FrontendSocket())
     controller.register(TTSCosyvoice())
     controller.register(LLMMiniLM2())
+    controller.register(BiliBiliChat())
     controller.start()
