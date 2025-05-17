8667f4a4f1e7b49eedca9600b3bd80ee91666261
kyv001 <kyvyang@foxmail.com>
2025-05-17 16:57:37 +0800

    去除多余的终端输出

--- swarmclone/__main__.py
+++ swarmclone/__main__.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     controller.register(FrontendSocket())
     controller.register(TTSCosyvoice())
     controller.register(LLMMiniLM2())
-    controller.register(BiliBiliChat())
+    # controller.register(BiliBiliChat())
     controller.start()
