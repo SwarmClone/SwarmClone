bd24880b73ee55d8adf2246f517196727e4c0e7a
HH2333333 <HH233333334@gmail.com>
2025-06-04 01:02:58 +0800

    实现了新框架下的asr服务端

--- swarmclone/__main__.py
+++ swarmclone/__main__.py
@@ -6,13 +6,15 @@ from .frontend_socket import FrontendSocket
 from .llm_minilm2 import LLMMiniLM2
 from .bilibili_chat import BiliBiliChat
 from .asr_remote import ASRRemote
+from .asr import ASRSherpa
 
 if __name__ == "__main__":
     ## TODO：从命令行接收配置文件、模块列表等参数
-    controller = Controller(config=Config("config/custom_settings.toml"))
+    controller = Controller(config=Config())#("config/custom_settings.toml"))
     controller.register_module(FrontendSocket)
-    controller.register_module(TTSCosyvoice)
-    controller.register_module(LLMMiniLM2)
-    controller.register_module(BiliBiliChat)
+    # controller.register_module(TTSCosyvoice)
+    # controller.register_module(LLMMiniLM2)
+    # controller.register_module(BiliBiliChat)
     # controller.register_module(ASRRemote)
+    controller.register_module(ASRSherpa)
     controller.start()
