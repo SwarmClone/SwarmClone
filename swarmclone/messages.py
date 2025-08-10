f35dab25482d8df85f7155879d65db824a0c3746
kyv001 <kyvyang@foxmail.com>
2025-08-10 21:14:55 +0800

    防止试图将精确指定的模块类当枚举项使用

--- swarmclone/messages.py
+++ swarmclone/messages.py
@@ -71,7 +71,13 @@ class Message:
             "send_time": self.send_time,
             "message_type": self.message_type.value,
             "message_source": self.source.name,
-            "message_destinations": [destination.value for destination in self.destinations],
+            "message_destinations": [
+                        destination.value
+                    if isinstance(destination, ModuleRoles)
+                    else
+                        get_type_name(destination)
+                for destination in self.destinations
+            ],
             "message": [
                 {"key": k, "value": repr(v)}
                 for k, v in self.kwargs.items()
