19afcc7b85f40c2773c6d0456b82ce881bc5b960
郝某人BH <1218271192@qq.com>
2025-08-22 13:26:34 +0800

    添加带边距的格式化打印

--- swarmclone/__main__.py
+++ swarmclone/__main__.py
@@ -1,7 +1,9 @@
 from . import *
 
 if __name__ == "__main__":
-    print(f"{module_classes=}")
+
+    output = print_with_margin(module_classes, title="模块列表")
+    log.opt(raw=True).info(f"{output}")
+
     controller = Controller()
     controller.run()
-    pass
