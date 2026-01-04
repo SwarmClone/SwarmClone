6e7859b488444748eb34e0beec0599ae3e18489c
郝某人BH <1218271192@qq.com>
2026-01-04 21:05:58 +0800

    feat: 前端集成 primevue ，完善后端目录结构

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,18 @@
-import { createApp } from "vue";
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import {createApp} from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        ripple: true,
+        options: {
+            darkModeSelector: 'system',
+        }
+    }
+ });
+
+app.mount('#app');
