397902b83d99b20379cc1d9fb9ee1b7d2070b984
郝某人BH <1218271192@qq.com>
2026-04-28 21:25:28 +0800

    chore: 修复导入错误，简化现阶段的App.vue

--- dashboard/src/renderer/src/App.vue
+++ dashboard/src/renderer/src/App.vue
@@ -3,22 +3,16 @@ const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-    and
-    <span class="ts">TypeScript</span>
+  <h1 class="creator">Powered by electron-vite</h1>
+  <br>
+  <div align="center">
+    <h3>
+      Build an Electron app with
+      Vue
+      and
+      TypeScript
+    </h3>
+    <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+    <button @click="ipcHandle">Send IPC</button>
   </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
 </template>
